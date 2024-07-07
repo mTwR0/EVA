1d2f89f05f87bf5ee057b4f2b1ecd949d96f76fd
mTwR0 <147711036+mTwR0@users.noreply.github.com>
2024-07-07 23:33:57 +0300

    Update voice_generation.py

--- voice_generation.py
+++ voice_generation.py
@@ -1,10 +1,10 @@
 from TTS.utils.synthesizer import Synthesizer
 
 def generate_audio(text, output_path):
-    model_path = r"C:\Users\STAR\AppData\Local\tts\tts_models--en--ljspeech--tacotron2-DDC\model_file.pth"
-    config_path = r"C:\Users\STAR\AppData\Local\tts\tts_models--en--ljspeech--tacotron2-DDC\config.json"
-    voc_path = r"C:\Users\STAR\AppData\Local\tts\vocoder_models--en--ljspeech--hifigan_v2\model_file.pth"
-    voc_config_path = r"C:\Users\STAR\AppData\Local\tts\vocoder_models--en--ljspeech--hifigan_v2\config.json"
+    model_path = r""
+    config_path = r""
+    voc_path = r""
+    voc_config_path = r""
 
     syn = Synthesizer(
         tts_checkpoint=model_path,
